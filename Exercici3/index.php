9c6e80e1c1ba6bda06723910018a9ed6d44f2d7f
CotofluxNew <cotoflux@gmail.com>
2023-01-11 10:44:46 +0100

    Fet el Exercici 4

---
+++ Exercici3/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercici3</title>
+</head>
+<body>
+    <?php
+        $meuNom = "Núria";
+        echo "<h1>Hola soc la ".$meuNom. "</h1>";
+    ?>
+</body>
+</html>
